0e02ad6f435b3bcc8a36ee35c5502f316726d54f
lixu <lixu_sx@qiyi.com>
2017-08-21 21:22:30 +0800

    auto create new branch

---
+++ build/Pods.build/Release-iphoneos/Yoga.build/Objects-normal/arm64/YGEnums.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/iqiyi/Downloads/Develop/Developer/PrivatePods/dennis-li/Yoga/RNComponents/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.c \
+  /Users/iqiyi/Downloads/Develop/Developer/PrivatePods/dennis-li/Yoga/Pods/Target\ Support\ Files/Yoga/Yoga-prefix.pch \
+  /Users/iqiyi/Downloads/Develop/Developer/PrivatePods/dennis-li/Yoga/RNComponents/node_modules/react-native/ReactCommon/yoga/yoga/YGEnums.h \
+  /Users/iqiyi/Downloads/Develop/Developer/PrivatePods/dennis-li/Yoga/RNComponents/node_modules/react-native/ReactCommon/yoga/yoga/YGMacros.h
